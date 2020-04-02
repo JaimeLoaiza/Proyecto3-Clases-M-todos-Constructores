2755ab1947d4f066c2bf91bc69826bc2b6be3a64
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-04-01 20:42:05 -0500

    Proyecto3-Clases-Metodos-Constructores

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClasesMetodosyConstructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo mivehiculo = new Vehiculo("Renault","SMV639",2011);
+            Console.WriteLine("La marca del Vehiculo es: " + mivehiculo.marca);
+            Console.WriteLine("La placa del Vehiculo es: " + mivehiculo.placa);
+            Console.WriteLine("El modelo del Vehiculo es: " + mivehiculo.modelo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClasesMetodosyConstructores
+{
+    class Vehiculo
+    {
+        //Propiedades
+        public string marca;
+        public string placa;
+        public int modelo;
+        //Constructor
+        public Vehiculo(string marca, string placa, int modelo)
+        {
+            this.marca = marca;
+            this.placa = placa;
+            this.modelo = modelo;
+        }
+    }
+}
